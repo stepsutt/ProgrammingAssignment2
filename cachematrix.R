51af3ef71c527d2c99c34f1cde82b9b816c6ccd0
Stephen Sutton <steve.m.sutton@gmail.com>
2014-07-16 14:17:19 +0100

    First commit of function.

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,21 @@
 ## functions do
 
 ## Write a short comment describing this function
+##adadas
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(sol) m <<- sol
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 
 }
 
@@ -12,4 +25,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+    
 }
